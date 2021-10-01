3d7ed4a9fa5944e25618eaad905cf544691863c6
parmcoder <possawat2017@gmail.com>
2021-10-01 09:47:43 +0700

    fixed lint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-  root: true,
   env: {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended"],
   rules: {
     quotes: ["error", "double"],
   },
+  parserOptions: {
+    ecmaVersion: 8 // or 2017
+  }
 };
