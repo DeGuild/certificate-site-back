0ece05055dccf2ae3f639f5bd25483b62e5b2b64
parmcoder <possawat2017@gmail.com>
2021-10-06 21:38:12 +0700

    all CRUDs have been provided

--- functions/index.js
+++ functions/index.js
@@ -12,16 +12,11 @@ exports.addCertificate = functions.https.onRequest(async (req, res) => {
     ? req.body.url
     : "https://firebasestorage.googleapis.com/v0/b/deguild-2021.appspot.com/o/0.png?alt=media&token=131e4102-2ca3-4bf0-9480-3038c45aa372";
   // Push the new message into Firestore using the Firebase Admin SDK.
-   await admin
+  await admin
     .firestore()
     .collection("certificate/")
     .doc(`${address}`)
-    .set({ url });
-   await admin
-    .firestore()
-    .collection("deGuild/skills/addresses/")
-    .doc(`${address}`)
-    .set({ address });
+    .set({ url, address });
 
   // Send back a message that we've successfully written the message
   res.json({
@@ -42,5 +37,65 @@ exports.readCertificate = functions.https.onRequest(async (req, res) => {
 
   res.json({
     imageUrl: `${readResult.data().url}`,
+  });
+});
+
+exports.deleteCertificate = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const address = req.body.address;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  await admin.firestore().collection("certificate/").doc(`${address}`).delete();
+
+  // Send back a message that we've successfully written the message
+  res.json({
+    result: "Successful",
+  });
+});
+
+exports.allCertificates = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const lastItem = req.path;
+  let data = [];
+  if (lastItem.length > 1) {
+    const paths = lastItem.split("/");
+    if (paths[2] === "next") {
+      const startAtSnapshot = admin
+        .firestore()
+        .collection("certificate/")
+        .orderBy("address", "desc")
+        .startAfter(paths[1]);
+
+      const items = await startAtSnapshot.limit(8).get();
+      items.forEach((doc) => {
+        data.push(doc.id);
+      });
+    } else if (paths[2] === "previous") {
+      const startAtSnapshot = admin
+        .firestore()
+        .collection("certificate/")
+        .orderBy("address", "asc")
+        .startAfter(paths[1]);
+
+      const items = await startAtSnapshot.limit(8).get();
+      items.forEach((doc) => {
+        data.push(doc.id);
+      });
+    }
+  } else {
+    const readResult = await admin
+      .firestore()
+      .collection("certificate/")
+      .orderBy("address", "desc")
+      .limit(8)
+      .get();
+    // Send back a message that we've successfully written the message3
+    readResult.forEach((doc) => {
+      data.push(doc.id);
+    });
+    // readResult.map
+    functions.logger.log(readResult);
+  }
+  res.json({
+    result: data,
   });
 });
