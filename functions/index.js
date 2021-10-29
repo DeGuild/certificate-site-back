9c704134ba559fed71e81b7ccb303897e7a423c9
parmcoder <possawat2017@gmail.com>
2021-10-29 21:22:20 +0700

    authorized all endpoints

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by certificatelicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,17 +17,46 @@
 
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
+admin.initializecertificate();
 const express = require("express");
 //  const cookieParser = require('cookie-parser')();
 const cors = require("cors")({ origin: true });
-const app = express();
+const certificate = express();
+
+
+const validateWeb3Token = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    functions.logger.error(
+      "No web token was passed in the Authorization header."
+    );
+    res.status(403).send("Unauthorized");
+    return;
+  }
+
+  const token = req.headers.authorization;
+
+  try {
+    const { address, body } = await Web3Token.verify(token);
+    if (
+      address === "0xAe488A5e940868bFFA6D59d9CDDb92Da11bb2cD9" ||
+      address === "0x785867278139c1cA73bF1e978461c8028061aDf6" ||
+      req.originalUrl === "/test"
+    ) {
+      next();
+      return;
+    }
+  } catch (error) {
+    functions.logger.error("Error while verifying Firebase ID token:", error);
+  }
+  res.status(403).send("Unauthorized");
+  return;
+};
 
 const addCertificate = async (req, res) => {
   const address = req.body.address;
   const url = req.body.url
     ? req.body.url
-    : "https://firebasestorage.googleapis.com/v0/b/deguild-2021.appspot.com/o/0.png?alt=media&token=131e4102-2ca3-4bf0-9480-3038c45aa372";
+    : "https://firebasestorage.googleapis.com/v0/b/deguild-2021.certificatespot.com/o/0.png?alt=media&token=131e4102-2ca3-4bf0-9480-3038c45aa372";
   // Push the new message into Firestore using the Firebase Admin SDK.
   await admin
     .firestore()
@@ -117,13 +146,14 @@ const allCertificates = async (req, res) => {
   });
 };
 
-app.use(cors);
-app.post("/addCertificate", addCertificate);
-app.get("/readCertificate/:address", readCertificate);
-app.post("/deleteCertificate", deleteCertificate);
-app.get("/allCertificates/:address/:direction", allCertificates);
-app.get("/allCertificatesOnce", allCertificates);
-// app.use(cookieParser);
-// app.use(validateFirebaseIdToken);
+certificate.use(cors);
+certificate.use(validateWeb3Token);
+certificate.post("/addCertificate", addCertificate);
+certificate.get("/readCertificate/:address", readCertificate);
+certificate.post("/deleteCertificate", deleteCertificate);
+certificate.get("/allCertificates/:address/:direction", allCertificates);
+certificate.get("/allCertificatesOnce", allCertificates);
+// certificate.use(cookieParser);
+// certificate.use(validateFirebaseIdToken);
 
-exports.app = functions.https.onRequest(app);
+exports.certificate = functions.https.onRequest(certificate);
