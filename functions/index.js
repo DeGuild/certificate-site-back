3d82ca458c99d7267b9c563c5f23c452685f7929
parmcoder <possawat2017@gmail.com>
2021-11-25 16:54:50 +0700

    adding data to backend only work for owner of the certificate

--- functions/index.js
+++ functions/index.js
@@ -22,6 +22,9 @@ const express = require("express");
 //  const cookieParser = require('cookie-parser')();
 const cors = require("cors")({ origin: true });
 const certificate = express();
+const skillCertificatePlusABI =
+  require("./contracts/SkillCertificates/V2/ISkillCertificate+.sol/ISkillCertificatePlus.json").abi;
+const ownableABI = require("./contracts/Ownable.json").abi;
 
 const validateWeb3Token = async (req, res, next) => {
   if (!req.headers.authorization) {
@@ -36,11 +39,7 @@ const validateWeb3Token = async (req, res, next) => {
 
   try {
     const { address, body } = await Web3Token.verify(token);
-    if (
-      address === "0xAe488A5e940868bFFA6D59d9CDDb92Da11bb2cD9" ||
-      address === "0x785867278139c1cA73bF1e978461c8028061aDf6" ||
-      req.originalUrl === "/test"
-    ) {
+    if (address) {
       next();
       return;
     }
@@ -51,6 +50,45 @@ const validateWeb3Token = async (req, res, next) => {
   return;
 };
 
+const addCertificateWeb3 = async (req, res) => {
+  const web3 = createAlchemyWeb3(functions.config().web3.api);
+
+  const token = req.headers.authorization;
+  const { address, body } = await Web3Token.verify(token);
+  const userAddress = web3.utils.toChecksumAddress(address);
+
+  const addressCertificate = req.body.address;
+  const tokenId = req.body.tokenId;
+  const title = req.body.title;
+  const url = req.body.url
+    ? req.body.url
+    : "https://firebasestorage.googleapis.com/v0/b/deguild-2021.certificatespot.com/o/0.png?alt=media&token=131e4102-2ca3-4bf0-9480-3038c45aa372";
+
+  const ownable = new web3.eth.Contract(ownableABI, addressCertificate);
+  const ownerOfManager = await ownable.methods.owner().call();
+  if (ownerOfManager !== userAddress) {
+    res.status(403).send("Unauthorized");
+    return;
+  }
+  await admin
+    .firestore()
+    .collection(`Certificate/`)
+    .doc(addressCertificate)
+    .set({ addressCertificate });
+
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  await admin
+    .firestore()
+    .collection(`Certificate/${addressCertificate}/tokens`)
+    .doc(tokenId)
+    .set({ url, addressCertificate, tokenId: parseInt(tokenId, 10), title });
+
+  // Send back a message that we've successfully written the message
+  res.json({
+    result: "Successful",
+  });
+};
+
 const addCertificate = async (req, res) => {
   const address = req.body.address;
   const tokenId = req.body.tokenId;
@@ -58,13 +96,13 @@ const addCertificate = async (req, res) => {
   const url = req.body.url
     ? req.body.url
     : "https://firebasestorage.googleapis.com/v0/b/deguild-2021.certificatespot.com/o/0.png?alt=media&token=131e4102-2ca3-4bf0-9480-3038c45aa372";
-    await admin
+  await admin
     .firestore()
     .collection(`Certificate/`)
     .doc(address)
-    .set({address});
+    .set({ address });
 
-    // Push the new message into Firestore using the Firebase Admin SDK.
+  // Push the new message into Firestore using the Firebase Admin SDK.
   await admin
     .firestore()
     .collection(`Certificate/${address}/tokens`)
@@ -92,6 +130,7 @@ const deleteCertificate = async (req, res) => {
 certificate.use(cors);
 // certificate.use(validateWeb3Token);
 certificate.post("/addCertificate", addCertificate);
+certificate.post("/addCertificateWeb3", addCertificateWeb3);
 certificate.post("/deleteCertificate", deleteCertificate);
 
 exports.certificate = functions.https.onRequest(certificate);
